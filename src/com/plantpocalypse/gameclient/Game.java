756bc844bad5715f1934a593be8413c208569466
Jeffrey J. Haywood <jeffreyjhaywood@gmail.com>
2020-08-27 14:05:03 -0700

    Add Player Object, Add logic for GameClient

--- src/com/plantpocalypse/gameclient/Game.java
+++ src/com/plantpocalypse/gameclient/Game.java
@@ -1,4 +1,55 @@
 package com.plantpocalypse.gameclient;
 
-public class Game {
+import com.plantpocalypse.Player;
+import com.plantpocalypse.Room;
+
+import java.util.HashMap;
+
+public enum Game {
+    GAME_INSTANCE;
+
+    Player player;
+    Room outside;
+    HashMap<String, Room> mansion;
+
+    /**
+     * Loads assets for the game to run properly:
+     * Instantiates Player, Rooms and other necessary assets.
+     */
+    public void loadAssets() {
+        player = new Player();
+        loadRooms();
+
+        System.out.println("Game has been loaded.");
+    }
+
+    private void loadRooms() {
+        String[] outsideAdjRooms = {"Foyer"};
+        outside = new Room("Outside", outsideAdjRooms);
+    }
+
+    /**
+     * Will
+     */
+    public void startGame() {
+        titleScreen();
+        introDialogue();
+
+        // loop until Player beats the game
+
+        endingDialogue();
+    }
+
+    private void titleScreen() {
+        System.out.println("This is the title screen");
+    }
+
+    private void introDialogue() {
+        System.out.println("This is the intro dialogue");
+    }
+
+    private void endingDialogue() {
+        System.out.println("This is the ending dialogue");
+    }
+
 }
