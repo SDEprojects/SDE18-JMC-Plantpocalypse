72a1ce4ab7bcc371dbeb413797aaa11ed0a4ad0b
Jeffrey J. Haywood <jeffreyjhaywood@gmail.com>
2020-09-08 13:42:25 -0700

    Add look command, Add look() which will display surrounding doors, monsters and items in Player's current room.

--- src/com/plantpocalypse/util/TextParser.java
+++ src/com/plantpocalypse/util/TextParser.java
@@ -40,7 +40,7 @@ public class TextParser {
 
     private static boolean checkValidInput(List<String> input) {
         boolean isValid = false;
-        List<String> oneWordCommands = Arrays.asList("inventory","help","quit");
+        List<String> oneWordCommands = Arrays.asList("look", "inventory","help","quit");
         List<String> twoWordCommands = Arrays.asList("go","eat","use","examine", "get");
         if (input.size() == 0) {
             System.out.println("Please enter command with correct format: command [arg]");
