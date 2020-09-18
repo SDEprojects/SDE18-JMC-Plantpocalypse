e2175f5a2e847a80ad8a6a12b7eb2155c37ffa7d
Michael <michaelvance0416@gmail.com>
2020-09-18 15:11:22 -0700

    several tests and test files added for Game class

--- test/com/plantpocalypse/model/RoomTest.java
+++ test/com/plantpocalypse/model/RoomTest.java
@@ -1,20 +1,20 @@
-package com.plantpocalypse.model;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class RoomTest {
-
-    @Test
-    void addItem() {
-    }
-
-    @Test
-    void getItem() {
-    }
-
-    @Test
-    void isLocked() {
-    }
-}
+//package com.plantpocalypse.model;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class RoomTest {
+//
+//    @Test
+//    void addItem() {
+//    }
+//
+//    @Test
+//    void getItem() {
+//    }
+//
+//    @Test
+//    void isLocked() {
+//    }
+//}
